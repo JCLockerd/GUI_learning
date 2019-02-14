4ca6505c1cfab156dfb009ea553ec225049b5339
jclockerd <lockerdju@wgc.school.nz>
2019-02-14 15:11:28 +1300

    Added a title and geometry to the page. Added a Label.

--- tKinter.py
+++ tKinter.py
@@ -1,5 +1,9 @@
 import tkinter  # Imports tkinter library
 root = tkinter.Tk()  # Creates a 'window'
-root.title("app")
+root.title("First App")
 root.geometry("500x300")
+my_lable = tkinter.Label(root)
+my_lable.config(text="Hey there!", fg="pink")
+my_lable.grid()
+
 root.mainloop()  # Starts GUI learning
