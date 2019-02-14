78c49ef0c1006a1391196f19d6b0ece4ed0943ba
jclockerd <lockerdju@wgc.school.nz>
2019-02-15 10:12:27 +1300

    Buttons

--- not_buttons.py
+++ not_buttons.py
@@ -1,7 +1,7 @@
 import tkinter
 root = tkinter.Tk()
 
-button1 = tkinter.Button(root)
+button1 = tkinter.Button(root) # hello
 button1.config(text="UWU what's this",
                bg="#FFA490",
                fg="#C4EFF8",
--- tKinter.py
+++ tKinter.py
@@ -4,11 +4,11 @@ root.title("First App")
 root.geometry("500x300")
 
 my_lable = tkinter.Label(root)
-my_lable.config(text="Hey there!", fg="pink")
+my_lable.config(text="uwu!", fg="#FFA490", bg="#C4EFF8")
 my_lable.grid()
 
 my_lable2 = tkinter.Label(root)
-my_lable2.config(text="How are you?", fg="green")
+my_lable2.config(text="UWU?", fg="#C9F8C4")
 my_lable2.grid()
 
 root.mainloop()  # Starts GUI learning
